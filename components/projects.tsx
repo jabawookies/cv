d4a2961d785013e59030501d504e88d9f3088d8d
jabawookies <81943673+jabawookies@users.noreply.github.com>
2024-10-04 12:48:38 +0300

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,7 +10,7 @@ export default function projects({ }: Props) {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+            className='h-auto relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
 
             <div className=' scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab8a] relative w-full flex overflow-x-scroll overflow-y-hidden snap-x
